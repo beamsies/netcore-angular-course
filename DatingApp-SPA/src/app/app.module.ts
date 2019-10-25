3f3f0107d0d117114d988691de130cbd0e10df83
Ben Siewert <ben.siewert86@gmail.com>
2019-10-24 22:16:02 -0600

    section 16, 165: complete

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -42,6 +42,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -102,6 +103,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MemberEditResolver,
     MemberListResolver,
     ListsResolver,
+    MessagesResolver,
     PreventUnsavedChanges,
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
   ],
